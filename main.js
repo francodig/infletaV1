e30b1272c94f2cec4ffc84be66dcf3e5348f33b9
franco <francodignani@hotmail.com>
2022-12-30 08:25:28 -0600

    new changes

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 
 const form = document.getElementById('form')
-let precioContado = document.getElementById("precioContado").value
-let precioCuotas = document.getElementById("precioCuotas").value
-let inflacion = document.getElementById("inflacion").value
-let cuotas = document.getElementById("cuotas").value 
 let submit = document.getElementById("submit")
 
-form.addEventListener('submit', valorPresente());
+submit.addEventListener('click', valorPresente);
 
-function valorPresente(precioCuotas,inflacion, cuotas){
+
+    function valorPresente(){
+    let precioContado = document.getElementById('precioContado').value
+    let precioCuotas = document.getElementById("precioCuotas").value
+    let inflacion = document.getElementById("inflacion").value
+    let cuotas = document.getElementById("cuotas").value 
+
     let precioEnCuotas= precioCuotas/cuotas
 
     if (cuotas==3){
@@ -45,5 +47,4 @@ function valorPresente(precioCuotas,inflacion, cuotas){
         return (precioEnCuotas/(1+inflacion))+ (precioEnCuotas/(1+inflacion)**2)+ (precioEnCuotas/(1+inflacion)**3)+ (precioEnCuotas/(1+inflacion)**4)+ (precioEnCuotas/(1+inflacion)**5)+ (precioEnCuotas/(1+inflacion)**6)+ (precioEnCuotas/(1+inflacion)**7)+ (precioEnCuotas/(1+inflacion)**8)+ (precioEnCuotas/(1+inflacion)**9)+ (precioEnCuotas/(1+inflacion)**10)+ (precioEnCuotas/(1+inflacion)**11)+ (precioEnCuotas/(1+inflacion)**12)+ (precioEnCuotas/(1+inflacion)**13)+ (precioEnCuotas/(1+inflacion)**14)+ (precioEnCuotas/(1+inflacion)**15)+ (precioEnCuotas/(1+inflacion)**16)+ (precioEnCuotas/(1+inflacion)**17)+ (precioEnCuotas/(1+inflacion)**18)+ (precioEnCuotas/(1+inflacion)**19)+ (precioEnCuotas/(1+inflacion)**20)+ (precioEnCuotas/(1+inflacion)**21)+ (precioEnCuotas/(1+inflacion)**22)+ (precioEnCuotas/(1+inflacion)**23)+ (precioEnCuotas/(1+inflacion)**24)+ (precioEnCuotas/(1+inflacion)**25)+ (precioEnCuotas/(1+inflacion)**26)+ (precioEnCuotas/(1+inflacion)**27)+ (precioEnCuotas/(1+inflacion)**28)+ (precioEnCuotas/(1+inflacion)**29)+ (precioEnCuotas/(1+inflacion)**30)+ (precioEnCuotas/(1+inflacion)**31)+ (precioEnCuotas/(1+inflacion)**32)+ (precioEnCuotas/(1+inflacion)**32)+ (precioEnCuotas/(1+inflacion)**34)+ (precioEnCuotas/(1+inflacion)**35)+ (precioEnCuotas/(1+inflacion)**36)+ (precioEnCuotas/(1+inflacion)**37)+ (precioEnCuotas/(1+inflacion)**38)+ (precioEnCuotas/(1+inflacion)**39)+ (precioEnCuotas/(1+inflacion)**40)+ (precioEnCuotas/(1+inflacion)**41)+ (precioEnCuotas/(1+inflacion)**42)+ (precioEnCuotas/(1+inflacion)**43)+ (precioEnCuotas/(1+inflacion)**44)+ (precioEnCuotas/(1+inflacion)**45)+ (precioEnCuotas/(1+inflacion)**46)+ (precioEnCuotas/(1+inflacion)**47)+ (precioEnCuotas/(1+inflacion)**48)
     }
  } 
-
 
